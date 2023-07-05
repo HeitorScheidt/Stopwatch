9b870bf7ed2d0ef945f51dab5aeffbbec15b6e1e
Heitor scheidt <heitor.scheidt@gmail.com>
2023-07-05 11:08:00 -0300

    finalizando

--- app.js
+++ app.js
@@ -3,10 +3,91 @@ let hours = 0;
 let minutes = 0;
 let seconds = 0;
 let milliseconds = 0;
-let interval;
 let count = 1;
 let datei;
 let datef
+var startTime; //controle o tempo de inicio
+let interval; //intervalo para controle
+var elapsedPausedTime = 0; //tempo decorrido durante a pausa
+var parcial = []
+var elapsedTime
+var c = 0;
+
+let inputStartDate;
+let inputEndDate;
+
+let dinamicDiv;
+let deleteIcon, editIcon, cancelIcon, saveIcon;
+
+//INICIA O CRONOMETRO
+function startFunc(){
+
+    if(!interval){
+        datei = new Date()
+        parcial.push(datei)
+        startTime = datei.getTime() - elapsedPausedTime
+        interval = setInterval(updateFunc, 1)
+    }
+}
+
+//PAUSA O CRONOMETRO
+function stopFunc(){
+    clearInterval(interval); //Clear the Stopwatch Interval
+    datef = new Date()
+    elapsedPausedTime = datef.getTime() - startTime
+    parcial.push(datef)
+    interval = null
+    getParcial()
+}
+
+//RESETA O CRONOMETRO
+function resetFunc(){
+
+    stopFunc();
+    elapsedPausedTime = 0;
+
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    count = 1;
+
+    hour.textContent = textFormat(hours)
+    minute.textContent = textFormat(minutes)
+    second.textContent = textFormat(seconds)
+    milli.textContent = textFormat(milliseconds)
+
+
+    divList = document.getElementsByClassName("parcial")
+    for(var j = divList.length-1; j>=0;j--){
+        divList[j].remove()
+    }
+    
+}
+
+//FAZ O UPDATE DOS VALORES DO CRONOMETRO
+function updateFunc(){
+    var currentTime = new Date().getTime()
+    elapsedTime = currentTime - startTime
+    seconds = ((elapsedTime / 1000) |0) % 60
+    minutes = ((elapsedTime / 1000 / 60) |0) % 60
+    hours = (elapsedTime / 1000 / 60 / 60) | 0
+    milliseconds = elapsedTime % 1000; 
+
+    hour.textContent = textFormat(hours)
+    minute.textContent = textFormat(minutes)
+    second.textContent = textFormat(seconds)
+    milli.textContent = textFormat(milliseconds)
+}
+
+/********** AÇÕES DOS BOTÕES ************/
+
+//DELETA A PARCIAL
+function removeFunc(event){
+    const divElement = event.target.closest('div');
+    if(divElement)
+    divElement.remove();
+}
 
 //DOM BUTTON ELEMENTS
 const startButton = document.getElementById("startButton")
@@ -27,90 +108,161 @@ pauseButton.addEventListener("click", stopFunc)
 resetButton.addEventListener("click", resetFunc)
 
 //START TIMER
-function startFunc(){
-    interval = setInterval(() => {  //Define a Time Interval (10 milliseconds)
 
-        milliseconds ++;
-
-        if(milliseconds == 100){
-            seconds++;
-            milliseconds = 0;
-        }
-
-        if(seconds == 60){
-            minutes++;
-            seconds = 0;
-        }
-
-        if(minutes == 60){
-            hours++;
-            minutes = 0;
-        }
-
-        hour.textContent = textFormat(hours)
-        minute.textContent = textFormat(minutes)
-        second.textContent = textFormat(seconds)
-        milli.textContent = textFormat(milliseconds)
-        datei = getDate();
-        
-    }, 10)
-}
 
 function textFormat(time){
     return time < 10 ? `0${time}` : time
 }
 
-function stopFunc(){
-    datef = getDate();
-    getParcial();
-    clearInterval(interval); //Clear the Stopwatch Interval
+
+function formatDateTime(date) {
+  let formattedDateTime =  (new Date(date.getTime() - date.getTimezoneOffset() * 60000).toISOString()).slice(0, -5);
+    
+  //alert(formattedDateTime)
+  return formattedDateTime;
+}
+  
+
+
+
+function getParcial() {
+  // Criar a nova Div
+  dinamicDiv = document.createElement("div");
+  dinamicDiv.classList.add("parcial")
+  dinamicDiv.id = count++
+  divId = dinamicDiv.id
+
+  // BOTAO DELETAR
+  const deleteButton = document.createElement("button");
+  deleteIcon = document.createElement("img");
+  deleteIcon.setAttribute("src", "delete-24.png");
+  deleteButton.appendChild(deleteIcon);
+  deleteButton.addEventListener("click",removeFunc);
+
+  //BOTAO EDITAR
+  const editButton = document.createElement("button");
+  editIcon = document.createElement("img");
+  editIcon.setAttribute("src", "ferramenta-lapis.png");
+  editButton.appendChild(editIcon);
+  editButton.addEventListener("click", editFunc);
+
+  //BOTAO CANCELAR
+  const cancelButton = document.createElement("button");
+  cancelIcon = document.createElement("img");
+  cancelIcon.setAttribute("src", "cancelar.png");
+  cancelButton.appendChild(cancelIcon);
+  cancelButton.addEventListener("click", cancelFunc);
+  
+
+  //BOTÃO SALVAR
+  const saveButton = document.createElement("button");
+  saveIcon = document.createElement("img");
+  saveIcon.setAttribute("src", "marca-de-verificacao.png");
+  saveButton.appendChild(saveIcon);
+  saveButton.addEventListener("click", saveFunc);
+
+
+
+  // Criar os inputs
+  inputStartDate = document.createElement("input");
+  inputEndDate = document.createElement("input");
+
+  inputStartDate.type = "datetime-local";
+  inputStartDate.value = formatDateTime(datei)
+
+  inputEndDate.type = "datetime-local";
+  inputEndDate.value = formatDateTime(datef)
+
+  //readOnlyFunc(true)
+
+  dinamicDiv.append(`Parcial ${divId} | `, inputStartDate, " até ", inputEndDate, "| ",deleteButton ," ",editButton," ",cancelButton," ",saveButton)
+  stops.appendChild(dinamicDiv);
+    
+
 }
 
-function resetFunc(){
-    clearInterval(interval); //Clear the Stopwatch Interval
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-    milliseconds = 0;
-    count = 1;
+function editFunc(event){
+    const divElement = event.target.closest('div');
+    const inputss = divElement.querySelectorAll("input")
+
+    for(let i = 0; i < inputss.length; i++){
+        inputss[i].readOnly = false
+        inputss[i].disabled = false
+        changeStyle(inputss[i], "white", "black")
+
+    }
+
+}
+
+
+function changeStyle(input, background, color){
+
+    input.style.backgroundColor = background
+    input.style.color = color
+}
+
+function cancelFunc(event){
+    const divElement = event.target.closest('div');
+    const inputss = divElement.querySelectorAll("input")
+
+    const dataFinal = parcial.pop();
+    const dataInical = parcial.pop();
+
+    console.log(dataInical)
+    console.log(dataFinal)
+
+    for(let i = 0; i < inputss.length; i++){
+
+        if(i===0){
+            inputss[i].value = formatDateTime(dataInical)
+        }
+        if(i===1){
+            inputss[i].value = formatDateTime(dataFinal)
+        }
+
+        inputss[i].readOnly = false
+        inputss[i].disabled = false
+
+        changeStyle(inputss[i], "transparent", "white")
+    }
+}
+
+function saveFunc(event){
+    let currentdate = new Date();
+    let current = formatDateTime(currentdate)
+
+    const dateUm = inputStartDate.value;
+    const dateDois = inputEndDate.value;
+
+    c = datef - datei 
+    seconds = ((c / 1000) |0) % 60
+    minutes = ((c / 1000 / 60) |0) % 60
+    hours = (c / 1000 / 60 / 60) | 0
+    milliseconds = c % 1000; 
 
     hour.textContent = textFormat(hours)
     minute.textContent = textFormat(minutes)
     second.textContent = textFormat(seconds)
     milli.textContent = textFormat(milliseconds)
 
+    console.log(c)
 
-    divList = document.getElementsByClassName("parcial")
-    for(var j = divList.length-1; j>=0;j--){
-        divList[j].remove()
+
+    if (dateUm > current || dateDois > current) {
+        alert("ERRO: As datas não podem ser no futuro");
+    } else {
+        const divElement = event.target.closest('div');
+        const inputss = divElement.querySelectorAll("input")
+
+        for(let i = 0; i < inputss.length; i++){
+            inputss[i].readOnly = false
+            inputss[i].disabled = false
+
+            changeStyle(inputss[i], "transparent", "white")
+        }
     }
-    
 }
 
-function getParcial(){
-    let newDiv = document.createElement("div")  //Criate a new Div
-    let newTr = document.createElement("tr")    //Criate a new Tr
-    let newTh = document.createElement("th")    //Criate a new Th
-    let icon = document.createElement("img")    //Criate a new Image
-
-    newDiv.classList.add("parcial")             //Add a class ate the new Div
-    newTh.classList.add("new")                  //Add a class ate the new Th, just for style
-
-    icon.setAttribute("src", "delete-24.png")   //Add a src attribute at image
-
-    /*REMOVE FUNCTION*/
-    icon.onclick = function() {
-        newTh.remove();
-    }
-
-    newTh.textContent = `Parcial ${count++}| ${datei} até  ${datef} - ${dayPorcentage().toFixed(3)}% | `;    //text in th
-    
-    /*Add the dinamics HTML tags at screen*/
-    newTh.appendChild(icon);
-    newTr.appendChild(newTh);
-    newDiv.appendChild(newTr);
-    stops.appendChild(newDiv);
-}
 
 function dayPorcentage(){
     let totalSecondsInADay = 24 * 60 * 60   //Total seconds in a day
@@ -119,8 +271,3 @@ function dayPorcentage(){
     return (totalSecondsAtTimer/totalSecondsInADay) *100    //day porcentage
 }
 
-function getDate(){
-    var currentDate = new Date()
-    let dateToString = currentDate.toLocaleString() 
-    return dateToString
-}
